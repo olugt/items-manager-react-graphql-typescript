8c631bcba8befcf01dfc23b13be86be2e7f380dc
olugt <30696063+olugt@users.noreply.github.com>
2021-05-18 03:34:22 +0100

    Added magic strings enum.

---
+++ src/common/enumerations/MagicStringsEnum.ts
@@ -0,0 +1,9 @@
+
+/**
+ * This helps to avoid hard-coding magic strings.
+ */
+export enum MAGIC_STRINGS {
+    developmentEnvironment = "development",
+    productionEnvironment = "production",
+    redirectUrlQueryParameter = "redirect-url",
+}
