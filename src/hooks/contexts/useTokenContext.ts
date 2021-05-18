7ba8e3b9ae3ab1299f0a59d96ddb5ae4a415b8fd
olugt <30696063+olugt@users.noreply.github.com>
2021-05-18 13:42:17 +0100

    Introduced token context.

---
+++ src/hooks/contexts/useTokenContext.ts
@@ -0,0 +1,46 @@
+import { TokenContext } from "../../App";
+import { isTokenValid } from "../../common/logic/identityLogic";
+import { checkCookieExists, setCookie, unSetCookie, getCookie } from "../../common/logic/browserLogic";
+import { useProcessCustomContext } from "./customContextHooks";
+import { MAGIC_STRINGS } from "../../common/enumerations/MagicStringsEnum";
+
+/**
+ * Hook used to manage the TokenContext.
+ * @returns Object containing token model and a callback function to set it, as state and setState.
+ */
+export default function useTokenContext() {
+    const identityCookieName = MAGIC_STRINGS.identityTokenCookieName;
+
+    let returnValue = useProcessCustomContext(
+        TokenContext,
+   
+        (tokenDetailContextModel) => {
+
+            try {
+                const tokenCookie = getCookie(identityCookieName);
+                tokenDetailContextModel!.fromParsedJson(JSON.parse(tokenCookie!)); // JSON.parse function may throw error if the cookie had been saved as undefined, etc. that are not valid JSON.
+            } catch {
+                tokenDetailContextModel = null;
+            }
+
+            return tokenDetailContextModel;
+
+        },
+        () => {
+
+            return checkCookieExists(identityCookieName);
+
+        },
+        
+        (tokenDetailContextModel) => {
+
+            if (isTokenValid(tokenDetailContextModel)) {
+                setCookie(identityCookieName, JSON.stringify(tokenDetailContextModel), tokenDetailContextModel!.expiryDatetime!);
+            } else {
+                unSetCookie(identityCookieName);
+            }
+            return tokenDetailContextModel;
+
+        });
+    return returnValue;
+}
